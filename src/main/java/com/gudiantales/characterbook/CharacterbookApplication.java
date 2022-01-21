500cb6cccd255bdda5372105242abe752dd2e30c
kang <dkemfjqek@gmail.com>
2022-01-21 15:18:30 +0900

    first commit

---
+++ src/main/java/com/gudiantales/characterbook/CharacterbookApplication.java
@@ -0,0 +1,13 @@
+package com.gudiantales.characterbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CharacterbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gudiantales/characterbook/CharacterbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gudiantales.characterbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
