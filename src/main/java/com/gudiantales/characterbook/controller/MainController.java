af0683b624f91c16a29e03c0307d278250ac68a2
kang <dkemfjqek@gmail.com>
2022-02-06 16:57:08 +0900

    react add

--- src/main/java/com/gudiantales/characterbook/controller/MainController.java
+++ src/main/java/com/gudiantales/characterbook/controller/MainController.java
@@ -2,14 +2,16 @@ package com.gudiantales.characterbook.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 public class MainController {
-    @RequestMapping("/")
+    @GetMapping("/home")
     public String home(){
         log.info("main controller");
-        return "main";
+        return "Hello world!!";
     }
 }
