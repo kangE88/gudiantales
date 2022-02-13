ef703afd518650d7928a9f5ab35114db6acfc159
seowon <falazure@naver.com>
2022-02-14 00:09:51 +0900

    티니아

--- src/main/java/com/gudiantales/characterbook/character/CharacterStatus.java
+++ src/main/java/com/gudiantales/characterbook/character/CharacterStatus.java
@@ -1,6 +1,7 @@
 package com.gudiantales.characterbook.character;
 
 import com.gudiantales.characterbook.common.CommonEntity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,8 @@ public class CharacterStatus extends CommonEntity {
     private Long id;
 
     private String name;
+
+    private String weapon;
 
     @Enumerated(EnumType.STRING)
     private PropertyType propertyType;
--- src/main/java/com/gudiantales/characterbook/controller/MainController.java
+++ src/main/java/com/gudiantales/characterbook/controller/MainController.java
@@ -1,10 +1,14 @@
 package com.gudiantales.characterbook.controller;
 
+import com.gudiantales.characterbook.character.CharacterStatus;
+import com.gudiantales.characterbook.character.PropertyType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManagerFactory;
 
 @RestController
 @Slf4j
@@ -12,6 +16,15 @@ public class MainController {
     @GetMapping("/home")
     public String home(){
         log.info("main controller");
+        CharacterStatus cs = new CharacterStatus();
+        cs.setPropertyType(PropertyType.DARK);
+
+        cs.setName("티니아");
+        cs.setPropertyType(PropertyType.EARTH);
+        cs.setWeapon("사릉가");
+        //EntityManagerFactory emf = new
+
+
         return "123Hello world!!!!!";
     }
 }
