e068a5c73725c587665bddcff394cb3be5f1f7e8
kang <dkemfjqek@gmail.com>
2022-03-14 00:55:19 +0900

    임시 index 페이지 접근 시 캐릭터 정보 저장 하여 목록보는 페이지 확인 완료

--- src/main/java/com/gudiantales/characterbook/controller/MainController.java
+++ src/main/java/com/gudiantales/characterbook/controller/MainController.java
@@ -6,39 +6,48 @@ import com.gudiantales.characterbook.service.CharacterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.List;
 
 @Controller
 @Slf4j
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class MainController {
-//    private final CharacterService characterService;
-//
-//    @GetMapping("/home")
-//    public String home(){
-//        log.info("main controller");
-//        CharacterStatus cs = new CharacterStatus();
-//        cs.setPropertyType(PropertyType.DARK);
-//
-//        cs.setName("티니아");
-//        cs.setName("티니아2");
-//        cs.setPropertyType(PropertyType.EARTH);
-//        cs.setWeapon("사릉가");
-////        EntityManagerFactory emf = new
-//
-//        characterService.saveCharacter(cs); // 저장
-//
-//        return "123Hello world!!!!!";
-//
-//    }
+    private final CharacterService characterService;
+
+    @GetMapping("/")
+    public String index(){
+        log.info("main controller");
+        CharacterStatus cs = new CharacterStatus();
+        cs.setPropertyType(PropertyType.DARK);
+
+        cs.setName("티니아");
+        cs.setPropertyType(PropertyType.EARTH);
+        cs.setWeapon("사릉가");
+
+        characterService.saveCharacter(cs); // 저장
+
+        return "index";
+
+    }
 
     @GetMapping("/addCharacter")
     public String addCharacter(){
         return "main";
+    }
+
+    @GetMapping("/selectCharacter")
+    public String selectCharacter(Model model) {
+        List<CharacterStatus> characters = characterService.findCharacters();
+        model.addAttribute("characters", characters);
+        log.debug("characters::", characters);
+        return "character/characterList";
     }
 
 
